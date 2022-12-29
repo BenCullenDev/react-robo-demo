cf0d06d0b039fc8e1465206927ff38b8855404e2
Ben Culen <benjaminjcullen1@gmail.com>
2022-12-29 14:40:15 +0000

    Updates

---
+++ precache-manifest.d3b0eb7e2160fcb500d1ed6276ba5d47.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "a3e2ce6695a4834f8233",
+    "url": "/react-robo-demo/index.html/static/css/main.3da7ac3b.chunk.css"
+  },
+  {
+    "revision": "a3e2ce6695a4834f8233",
+    "url": "/react-robo-demo/index.html/static/js/main.a3e2ce66.chunk.js"
+  },
+  {
+    "revision": "84cf99745ce0980187aa",
+    "url": "/react-robo-demo/index.html/static/js/1.84cf9974.chunk.js"
+  },
+  {
+    "revision": "3c11059de640cd1ca19a",
+    "url": "/react-robo-demo/index.html/static/css/2.570ba6c1.chunk.css"
+  },
+  {
+    "revision": "3c11059de640cd1ca19a",
+    "url": "/react-robo-demo/index.html/static/js/2.3c11059d.chunk.js"
+  },
+  {
+    "revision": "4f385275ed1f3a32c60f",
+    "url": "/react-robo-demo/index.html/static/js/runtime~main.4f385275.js"
+  },
+  {
+    "revision": "252f6348bfd7df7340c2e42792ef6e6c",
+    "url": "/react-robo-demo/index.html/static/media/Pastor of Muppets.252f6348.TTF"
+  },
+  {
+    "revision": "9dfd211deb3d4355f64e3bc9a8bb39c5",
+    "url": "/react-robo-demo/index.html/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.bdf2b5724addfdedcbfdb5ba40c8cf95.js"
+  "/react-robo-demo/index.html/precache-manifest.d3b0eb7e2160fcb500d1ed6276ba5d47.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/react-robo-demo/index.html/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
