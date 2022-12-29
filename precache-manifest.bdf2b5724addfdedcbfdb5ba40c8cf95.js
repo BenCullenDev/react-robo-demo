e22b447be42ab84b63219f7baa48928f05e2566a
Ben Culen <benjaminjcullen1@gmail.com>
2022-12-29 14:27:15 +0000

    Updates

---
+++ precache-manifest.bdf2b5724addfdedcbfdb5ba40c8cf95.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "35b3f8f8cf803fc701f5",
+    "url": "/static/css/main.bfe4257a.chunk.css"
+  },
+  {
+    "revision": "35b3f8f8cf803fc701f5",
+    "url": "/static/js/main.35b3f8f8.chunk.js"
+  },
+  {
+    "revision": "84cf99745ce0980187aa",
+    "url": "/static/js/1.84cf9974.chunk.js"
+  },
+  {
+    "revision": "0e170053a5b060f98473",
+    "url": "/static/css/2.570ba6c1.chunk.css"
+  },
+  {
+    "revision": "0e170053a5b060f98473",
+    "url": "/static/js/2.0e170053.chunk.js"
+  },
+  {
+    "revision": "0b9ef2e8599e90f9bb62",
+    "url": "/static/js/runtime~main.0b9ef2e8.js"
+  },
+  {
+    "revision": "252f6348bfd7df7340c2e42792ef6e6c",
+    "url": "/static/media/Pastor of Muppets.252f6348.TTF"
+  },
+  {
+    "revision": "8e62eef396c07c2eecd86944ac8f408f",
+    "url": "/index.html"
+  }
+];
